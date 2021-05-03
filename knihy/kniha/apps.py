28d857a17c79158688444db5ec64ed3901dbb301
OndraMarek <czondrejmarek@gmail.com>
2021-05-03 17:14:59 +0200

    Modely

---
+++ knihy/kniha/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class KnihaConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'kniha'
---
+++ knihy/kniha/models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Zanry(models.Model):
+    zanr = models.CharField(max_length=20, unique=True, verbose_name="Žánr knihy",
+                            help_text='Zadejte text o maximální délce 20 znaků; text musí být jedinečný')
+
+    class Meta:
+        ordering = ["zanr"]
+        verbose_name = 'Žánr knihy'
+        verbose_name_plural = 'Žánr knihy'
+
+    def __str__(self):
+        return f"{self.zanr}"
+
+
+class Knihy(models.Model):
+    nazev = models.CharField(max_length=100, verbose_name="Název knihy", help_text='Zadejte text o maximální délce 100 znaků')
+    popis = models.TextField(blank=True, null=True, verbose_name="Popis knihy")
+    autor = models.CharField(max_length=100, null=True, verbose_name="Autor knihy",
+                             help_text='Zadejte text o maximální délce 100 znaků')
+    rok = models.IntegerField(null=True, help_text="Zadejte rok vydání knihy", verbose_name="Rok vydání")
+    foto = models.ImageField(upload_to='knihy/%Y/%m/%d/', blank=True, null=True, verbose_name="Fotka knihy")
+    zanry = models.ForeignKey(Zanry, on_delete=models.RESTRICT)
+
+    class Meta:
+        ordering = ["nazev"]
+        verbose_name = 'Knihy'
+        verbose_name_plural = 'Knihy'
+
+    def __str__(self):
+        return f"{self.nazev}"
+
+    def get_absolute_url(self):
+        """Metoda vrací URL stránky, na které se vypisují podrobné informace o zboží"""
+        return reverse('detail', args=[str(self.id)])
